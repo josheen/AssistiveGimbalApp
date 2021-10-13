beb6760d4b6cec1e64fceca19707bfebd4971e39
josheenyen99 <josheen@ualberta.ca>
2021-10-13 01:52:20 -0600

    Initial Commit

---
+++ AssistiveGimbalApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AssistiveGimbalApp
+//
+//  Created by Jo-Sheen Yen on 2021-10-13.
+//  Copyright © 2021 Jo-Sheen Yen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
